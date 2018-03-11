7b7362f194c227cc05ea3c599f4b2842278f9133
atzcl <atzcl0310@gmail.com>
2018-03-11 22:30:11 +0800

    增加 egg-router-plus、egg-validate 插件

--- config/plugin.js
+++ config/plugin.js
@@ -5,3 +5,14 @@ exports.jwt = {
   enable: true,
   package: 'egg-jwt',
 };
+
+// 挂载 egg-router-plus 路由拓展
+exports.routerPlus = {
+  enable: true,
+  package: 'egg-router-plus',
+};
+
+// 挂载 egg-validate 数据验证
+exports.validate = {
+  package: 'egg-validate',
+};
