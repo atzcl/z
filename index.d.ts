865c0936aa26f2878ad3abe5cc4eed149af89ac3
atzcl <atzcl0310@gmail.com>
2018-03-17 01:41:52 +0800

    增加第三方包，剥离 .d.ts 声明文件

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,26 @@
+// import { createBcrypt, verifyBcrypt } from './src/extend/utils/bcrypt'
+
 // config 配置声明
 export interface DefaultConfig {
   jwt: {
     secret: string,
     enable: boolean,
     match?: string
+  },
+  myApps: {
+    appName: string,
+    debug: boolean,
+    appUrl: string,
+    adminRouter: string,
+    mail_options: {
+      host: string,
+      port: number,
+      secure: boolean,
+      auth: {
+        user: string,
+        pass: string
+      }
+    }
   }
 }
 
@@ -13,7 +30,15 @@ declare module 'egg' {
     // 增加合并该项目的 config 声明
     config: EggAppConfig & DefaultConfig
     // jwt
-    jwt: any
+    jwt: any,
+    createBcrypt(value: string, salt?: number): string
+    verifyBcrypt(value: string, hash: string): boolean
+  }
+
+  // 拓展 egg 的 Context 对象
+  export interface Context {
+    // egg-validate 拓展的 validate 方法声明
+    validate(rules: object, data?: object): void
   }
 
   // 拓展 egg 的 Router 对象
@@ -24,7 +49,5 @@ declare module 'egg' {
 
   // 拓展 egg 的 app.helper 对象，导出项目编写的 Helper 给 TypeScript
   export interface IHelper {
-    bcrypt(value: string, salt?: number): string
-    checkBcrypt(value: string, hash: string): boolean
   }
 }
