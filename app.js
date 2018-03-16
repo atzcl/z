5e61dea3c4cdaabec839f3e32a136da9f1d5ebbd
atzcl <atzcl0310@gmail.com>
2018-03-17 01:41:06 +0800

    关闭 egg-sequelize 重置生成表

--- app.js
+++ app.js
@@ -4,4 +4,11 @@
 module.exports = app => {
   // 加载自定义 Loader
   require('./app/lib/loader')(app);
+
+  // 自动根据 model 定义自动生成数据表
+  // if (app.config.env === 'local') {
+  //   app.beforeStart(async () => {
+  //     await app.model.sync({ force: true });
+  //   });
+  // }
 };
