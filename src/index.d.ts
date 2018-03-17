5c6d15ae787a07a9d6ae106785fee865bf2e63a0
atzcl <atzcl0310@gmail.com>
2018-03-18 00:01:11 +0800

    将公共 .d.ts 文件移入 src 源码目录中，增加第三方包，增加 tslint 代码规范约束

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,5 @@
-// import { createBcrypt, verifyBcrypt } from './src/extend/utils/bcrypt'
+import { Redis }  from 'ioredis'
+import { VerifyOptions, Secret, SignOptions, DecodeOptions } from 'jsonwebtoken'
 
 // config 配置声明
 export interface DefaultConfig {
@@ -28,23 +29,44 @@ declare module 'egg' {
   // 拓展 egg 的 app 对象
   export interface Application {
     // 增加合并该项目的 config 声明
-    config: EggAppConfig & DefaultConfig
+    config: EggAppConfig & DefaultConfig;
     // jwt
-    jwt: any,
-    createBcrypt(value: string, salt?: number): string
-    verifyBcrypt(value: string, hash: string): boolean
+    jwt: {
+      // 加密
+      sign(
+        payload: string | Buffer | object,
+        secretOrPrivateKey: Secret,
+        options?: SignOptions,
+      ): string;
+
+      // 验证
+      verify(
+        token: string,
+        secretOrPublicKey: string | Buffer,
+        options?: VerifyOptions,
+      ): object | string;
+
+      // 解密
+      decode(
+        token: string,
+        options?: DecodeOptions,
+    ): null | { [key: string]: any } | string;
+    }
+    redis: Redis;
+    createBcrypt(value: string, salt?: number): string;
+    verifyBcrypt(value: string, hash: string): boolean;
   }
 
   // 拓展 egg 的 Context 对象
   export interface Context {
     // egg-validate 拓展的 validate 方法声明
-    validate(rules: object, data?: object): void
+    validate(rules: object, data?: object): void;
   }
 
   // 拓展 egg 的 Router 对象
   export interface Router {
     // egg-router-plus 拓展的 namespace 用法声明
-    namespace(prefix: string, ...middlewares: Function[]): Router
+    namespace(prefix: string, ...middlewares: Function[]): Router;
   }
 
   // 拓展 egg 的 app.helper 对象，导出项目编写的 Helper 给 TypeScript
