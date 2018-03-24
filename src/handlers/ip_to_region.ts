db1d9ad907e81a9c6e4f7491274e9bfb06d665d7
atzcl <atzcl0310@gmail.com>
2018-03-24 17:39:37 +0800

    增加 ip 地址信息查询

---
+++ src/handlers/ip_to_region.ts
@@ -0,0 +1,23 @@
+/*
++-----------------------------------------------------------------------------------------------------------------------
+| Author: 植成樑 <atzcl0310@gmail.com>  Blog：https://www.zcloop.com
++-----------------------------------------------------------------------------------------------------------------------
+| 通过 ip 获取地址信息
+|
+*/
+
+import BaseHandler from '../base_class/base_handler'
+
+// ip2region 地址库
+const IP2Region = require('ip2region')
+
+export default class IpToRegion extends BaseHandler {
+  /**
+   * 传入 ip 查询地址信息
+   */
+  public async search (ip?: string) {
+    ip = ip || (this.ctx.ip as string)
+
+    return (new IP2Region()).search(ip)
+  }
+}
