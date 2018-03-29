d13342d29e85a878bdf8d758ac01774ac8b4eb99
atzcl <atzcl0310@gmail.com>
2018-03-29 15:46:30 +0800

    完成文章分类以及拓展表

---
+++ migrations/20180328101306-create_article_categories_table.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (db, Sequelize) => {
+    const { INTEGER, JSON, BOOLEAN, DATE } = Sequelize;
+    await db.createTable('article_categories', {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      parent_id: {
+        type: INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        comment: '父级 id',
+      },
+      parent_id_array: {
+        type: JSON,
+        allowNull: true,
+        comment: '父级宗族谱 id',
+      },
+      is_show: {
+        type: BOOLEAN,
+        allowNull: false,
+        defaultValue: 1,
+        comment: '是否显示: 1 正常； 0 禁用',
+      },
+      order: {
+        type: INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        comment: '排序',
+      },
+      created_at: DATE, // 创建时间
+      updated_at: DATE, // 更新时间
+    },
+    {
+      indexes: [
+        {
+          method: 'BTREE',
+          fields: [ 'parent_id' ],
+        },
+      ],
+    });
+  },
+
+  down: async db => {
+    // 回滚
+    await db.dropTable('article_categories');
+  },
+};
