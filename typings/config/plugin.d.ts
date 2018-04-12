9218c99d523e52d13d4ca51348ccba267a03123c
atzcl <atzcl0310@gmail.com>
2018-04-13 00:09:36 +0800

    更新 plugin

---
+++ typings/config/plugin.d.ts
@@ -0,0 +1,11 @@
+// This file was auto created by egg-ts-helper
+// Do not modify this file!!!!!!!!!
+
+import 'egg-jwt';
+import 'egg-sequelize';
+import 'egg-router-plus';
+import 'egg-validate';
+import 'egg-redis';
+import 'egg-session-redis';
+import 'egg-socket.io';
+import 'egg-view-ejs';
