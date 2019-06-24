909edb33e0cffdc28dc83803ea3127ea63041486
atzcl <atzcl0310@gmail.com>
2019-06-24 16:31:12 +0800

    refactor(typeorm): 调整 typeorm cli config 设置

---
+++ ormconfig.ts
@@ -0,0 +1,30 @@
+/*
++-----------------------------------------------------------------------------------------------------------------------
+| Author: atzcl <atzcl0310@gmail.com>  https://github.com/atzcl
++-----------------------------------------------------------------------------------------------------------------------
+| typeorm cli config
+|
+*/
+
+import { customizeConfig } from './src/config/config.default';
+
+const dbConfig = customizeConfig.sequelize;
+
+const typeormConfig = {
+  type: dbConfig.dialect,
+  host: dbConfig.host,
+  port: dbConfig.port,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database:  dbConfig.database,
+  timezone: dbConfig.timezone,
+  logging: dbConfig.logging,
+  charset: 'UTF8MB4_UNICODE_CI',
+  entities: [ 'src/app/modules/**/Models/*{.ts,.js}' ],
+  migrations: [ 'src/app/modules/**/Migrations/*{.ts,.js}' ],
+  cli: {
+    migrationsDir: 'src/migration',
+  },
+};
+
+export = typeormConfig;
