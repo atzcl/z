1408bb7ffc98d1bff0701cc0ab08a414e25ce956
atzcl <atzcl0310@gmail.com>
2019-06-29 23:56:22 +0800

    refactor: 调整增加异常类

---
+++ src/app/exceptions/AppFlowException.ts
@@ -0,0 +1,15 @@
+/*
++-----------------------------------------------------------------------------------------------------------------------
+| Author: atzcl <atzcl0310@gmail.com>  https://github.com/atzcl
++-----------------------------------------------------------------------------------------------------------------------
+| 应用通用异常类
+|
+*/
+
+import { BaseException } from './BaseException';
+
+export class AppFlowException extends BaseException {
+  constructor(message: string, code: number) {
+    super('AppFlowException', code, message);
+  }
+}
